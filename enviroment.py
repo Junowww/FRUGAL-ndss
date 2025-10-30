359af93fb016315ee15c148050feae74e1290430
Wang Rong <58450422+Junowww@users.noreply.github.com>
2025-10-30 12:49:01 +0800

    Update enviroment.py

--- enviroment.py
+++ enviroment.py
@@ -12,34 +12,35 @@ class Environment():
 
     def get_single_trace_attackmodel_result(self, x_sample, y_sample):
         self.attack_net.eval()
-        _, ori_label_tensor = torch.max(y_sample.data, dim=-1)
-        ori_label = ori_label_tensor.item()
-        b_xnew = x_sample.reshape((1, 1, x_sample.shape[1]))
-        b_xnew = x_sample.repeat(2, 1, 1)
-        output = self.attack_net(b_xnew.float())
-        probs = torch.softmax(output[0], dim=-1)
-        max_value, max_index = torch.max(probs.data, dim=0)
-        prob_list = probs.tolist()
-        still_in_prob = prob_list[ori_label]
+        with torch.no_grad():
+            _, ori_label_tensor = torch.max(y_sample, dim=-1)
+            ori_label = int(ori_label_tensor.item())
+            b_xnew = x_sample.reshape((1, 1, x_sample.shape[1]))
+            b_xnew = x_sample.repeat(2, 1, 1)
+            output = self.attack_net(b_xnew.float())
+            probs = torch.softmax(output[0], dim=-1)
+            max_value, max_index = torch.max(probs, dim=0)
+            still_in_prob = probs[ori_label].item()
 
         return max_value, still_in_prob, probs
 
     def attack_reward(self, x_sample, y_sample, num):
-        x_sample = x_sample.reshape((1, 1, x_sample.shape[1]))
-        x_sample = x_sample.repeat(2, 1, 1)
-        prob = self.attack_net(x_sample)[0]
-        pred = torch.softmax(prob, dim=-1)
-        _, ori_max_index = torch.max(y_sample.data, dim=-1)
-        means = torch.tensor(0.0, device=pred.device)
-        for i in range(num):
-            if i == ori_max_index:
-                continue
-            else:
-                means = means + torch.log(1-pred[i])
-        mean = means / (num-1)
-        reward = torch.log(pred[ori_max_index]) - mean.detach()
-        positive_reward = torch.log(pred[ori_max_index])
-        negative_reward = (mean.detach())
+        with torch.no_grad():
+            x_sample = x_sample.reshape((1, 1, x_sample.shape[1]))
+            x_sample = x_sample.repeat(2, 1, 1)
+            prob = self.attack_net(x_sample)[0]
+            pred = torch.softmax(prob, dim=-1)
+            _, ori_max_index = torch.max(y_sample, dim=-1)
+            means = torch.tensor(0.0, device=pred.device)
+            for i in range(num):
+                if i == int(ori_max_index.item()):
+                    continue
+                else:
+                    means = means + torch.log(1 - pred[i])
+            mean = means / (num - 1)
+            reward = torch.log(pred[ori_max_index]) - mean
+            positive_reward = torch.log(pred[ori_max_index])
+            negative_reward = mean
         return reward, positive_reward, negative_reward
 
     def get_single_trace_environment_feedback(self, x_sample, y_sample, num):
@@ -48,20 +49,19 @@ class Environment():
         return max_prob, still_in_prob, reward
 
     def get_pred_label(self, traffic):
-        b_xnew = traffic.reshape((1, 1, traffic.shape[1]))
-        b_xnew = traffic.repeat(2, 1, 1)
-        output = self.attack_net(b_xnew)
-        probs = torch.softmax(output[1], dim=-1)
-        # _, pred_label = torch.argmax(output.data, dim=-1)
-        pred_label = torch.max(probs, 0)[1].data
-        pre_label = pred_label.tolist()
-        # print("pre_label:", pre_label)
-        return pre_label
+        with torch.no_grad():
+            b_xnew = traffic.reshape((1, 1, traffic.shape[1]))
+            b_xnew = traffic.repeat(2, 1, 1)
+            output = self.attack_net(b_xnew)
+            probs = torch.softmax(output[1], dim=-1)
+            pred_label = torch.max(probs, 0)[1]
+            pre_label = int(pred_label.item())
+            return pre_label
 
     def get_ori_label(self, traffic_idx):
-        ori_max_value, ori_max_index = torch.max(traffic_idx.data, dim=-1)
-        ori_max_index = ori_max_index.tolist()
-        return ori_max_index
+        with torch.no_grad():
+            _, ori_max_index = torch.max(traffic_idx, dim=-1)
+            return int(ori_max_index.item())
 
     def check_done(self, traffic, traffic_idx):
         pred_label = self.get_pred_label(traffic)
@@ -71,24 +71,19 @@ class Environment():
         else:
             return False 
     
-    def MI_update(self, traffic, traffic_idx, i, max_features=5):
-        predictor = self.attack_net
-        predictor.train()
-        predictor.zero_grad()
-
-        # estimator update
-        traffic = traffic.reshape((1, 1, traffic.shape[1]))
-        traffic = traffic.repeat(2, 1, 1)
-        pred = predictor(traffic)
-        loss = self.mi_loss_fn(pred[0], torch.argmax(traffic_idx, dim=0))
-        (loss / max_features).backward()
-
-        if i % max_features == 0:
+    def MI_update(self, traffic, traffic_idx, i, update_every=5):
+        # 使用 attack_net 进行交叉熵更新，降低更新频率
+        self.attack_net.train()
+        self.attack_net.zero_grad()
+        b_x = traffic.reshape((1, 1, traffic.shape[1]))
+        b_x = traffic.repeat(2, 1, 1)
+        logits = self.attack_net(b_x)[0]
+        target = torch.argmax(traffic_idx, dim=0)
+        loss = self.mi_loss_fn(logits, target)
+        (loss/update_every).backward()
+        if i % update_every == 0:
             self.mi_optimizer.step()
             self.mi_optimizer.zero_grad()
-
-
-
 
 
 
